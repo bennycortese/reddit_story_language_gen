ffa597cbb74b763d645219349042d700a08e0cbe
Benny Cortese <bennycortese@gmail.com>
2024-01-09 23:54:14 -0500

    more skeleton for this helpful little tool

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ reddit = praw.Reddit(
     username='',
     password=''
 )
+
+
+
+def story_grabber(num_stories):
+    # grab that amount of stories off of reddit, have done this elsewhere
+
+
+def french_translation(input_text):
+    # train LLM or use pretrained-LLM or neural network to do the french translation
