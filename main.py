93d85615ea88a0628ac462aa6e9122032e59f01f
benny <bennycortese@gmail.com>
2024-01-13 23:34:22 -0500

    Added CLIENT_ID stuff for reddit credentials

--- main.py
+++ main.py
@@ -1,41 +1,40 @@
 import praw
 import csv
-import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client_id=os.getenv("CLIENT_ID")
+client_secret=os.getenv("CLIENT_SECRET")
+user_agent=os.getenv("USER_AGENT")
+username=os.getenv("USERNAME_REDDIT")
+password=os.getenv("PASSWORD")
+
 
 reddit = praw.Reddit(
-    client_id='',
-    client_secret='',
-    user_agent='',
-    username='',
-    password=''
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent=user_agent,
+    username=username,
+    password=password
 )
+
 
 api_key = "YOUR_API_KEY_HERE"
 
 
 def story_grabber(num_stories):
+    stories_content = []
     subreddit = reddit.subreddit('WritingPrompts')
     hottest_stories = subreddit.hot(limit=50)
     for story in hottest_stories:
         story.comments.replace_more(limit=None)
         for comment in story.comments.list():
-            if comment.body:
+            if comment.body and comment.score > 100 and len(comment.body) > 1000:
                 translated_comment = french_translation(comment.body)
                 stories_content.append(translated_comment)
     return stories_content
 
-def query_gpt(prompt, api_key):
-    openai.api_key = api_key
-
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=150
-    )
-
-    return response.choices[0].text.strip()
-
-
-def french_translation(input_text, api_key):
-    # train LLM or use pretrained-LLM or neural network to do the french translation
-    return query_gpt(input_text, api_key)
+def eng_to_french(prompt):
+    return prompt
