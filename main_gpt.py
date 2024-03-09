1b06e9174cacb30c68d89f44598e45fff8513082
benny <bennycortese@gmail.com>
2024-03-08 23:57:19 -0500

    Anthropic key added!

--- main_gpt.py
+++ main_gpt.py
@@ -26,3 +26,25 @@ def complete_text(prompt):
         ]
     )
     return response.choices[0].message.content
+
+@stub.function(secret=modal.Secret.from_name("my-anthropic-secret"))
+def complete_text_anthropic(prompt):
+    import anthropic
+
+    client = anthropic.Anthropic(
+        # defaults to os.environ.get("ANTHROPIC_API_KEY")
+        api_key=os.environ["ANTHROPIC_API_KEY"],
+    )
+
+    message = client.messages.create(
+        model="claude-3-opus-20240229",
+        max_tokens=32000,
+        temperature=0.0,
+        system="Respond only in Yoda-speak.",
+        messages=[
+            {"role": "system", "content": "You are an instructor who is very good at grading."},
+            {"role": "user", "content": prompt},
+        ]
+    )
+
+    return message.content
